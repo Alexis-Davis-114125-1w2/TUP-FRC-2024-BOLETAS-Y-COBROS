06de6e302fd142b80cec2c2d565d0ba3983cd796
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-09-17 17:31:19 -0300

    entities

---
+++ Back/boleta-service/src/main/java/com/example/boleta_service/entities/BillDetail.java
@@ -0,0 +1,37 @@
+package com.example.boleta_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bill_details")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BillDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "bill_id", referencedColumnName = "id")
+    private Bill bill;
+
+    @ManyToOne
+    @JoinColumn(name = "expense_type_id", referencedColumnName = "id")
+    private ExpenseType expenseType;
+
+    private String description;
+    private Double amount;
+    private Double percentage;
+
+    private LocalDateTime createdDate;
+    private String createdUser;
+
+    private LocalDateTime lastUpdatedDate;
+    private String lastUpdatedUser;
+}
