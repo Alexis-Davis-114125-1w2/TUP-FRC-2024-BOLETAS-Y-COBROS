543ab28a1bc6157ee15c46dc1b96c94d7555ca7c
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-09-17 14:03:45 -0300

    scaffolding back

---
+++ Back/mercado-service/src/test/java/com/example/mercado_service/MercadoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercado_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
